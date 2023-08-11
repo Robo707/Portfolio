908547185429079610d51445fdf3b8983fe94869
Himanshu <himanshu.kumarr07@gmail.com>
2023-08-11 15:45:21 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+const About = () => {
+  return (
+    <motion.div className='mt-32 p-10 pt-12 h-auto flex flex-col justify-evenly items-start md:items-center'
+    initial={{ opacity: 0}}
+    animate={{ opacity: 1 }}
+    transition={{ delay: 0.3 }}>
+        <article className='grid grid-cols-1 md:items-center md:w-2/5 md:p-2'>
+            <h1 className='font-bold text-lg mb-8'>A lil' piece of me</h1>
+            <p className='text-sm'>
+                <span className='underline decoration-purple-600 decoration-4'>Curious & Creative</span>; that's how I would describe myself if I had only two words to do so.
+            </p>
+            <br></br>
+            <p className='text-sm'>
+              I am a full stack developer and UI/UX designer who never gets bored while coding and creating beautiful UI.
+            </p>
+            <br></br>
+            <p className='text-sm'>
+                  I'm a CSE undergrad('25) and I'm looking for <span className='underline decoration-purple-600 decoration-4'>internship for frontend or full stack role </span>in any early stage startup.
+            </p>
+            <br></br>
+              <p className='text-sm'>Hit me up on <a href='https://twitter.com/Himanshu_887' target='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>Twitter</a> or <a href='https://www.linkedin.com/in/himanshush70a' target='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>Linkedin</a>.</p>
+            <p className='text-sm'>Or send a <a href='mailto: himanshu.kumarr07@gmail.com' target='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>mail</a>.</p>
+            <br></br>
+            <p className='text-sm'>Thanks for reading :)</p>
+        </article>
+    </motion.div>
+  )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { FaGithubSquare as Github, FaTwitterSquare as Twitter, FaLinkedin as Linkedin } from 'react-icons/fa'
+import { SiLeetcode as Leetcode, SiCodechef as Codechef } from 'react-icons/si'
+import { motion } from 'framer-motion'
+
+const Home = () => {
+    return (
+        <motion.div
+            className="overflow-hidden App flex flex-col justify-center items-start md:items-center mt-32 md:mt-48 p-10 pt-12"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.2 }}>
+            <section
+                className='grid grid-cols-1 items-start md:grid-cols-2 md:items-center md:w-[60%]'>
+                <picture>
+                    <img src="/images/dp.jpg" alt='my profile' className='mb-4 w-32 h-32 rounded-full object-cover md:h-48 md:w-48 md:mb-0'></img>
+                </picture>
+
+                <article>
+
+                    <h1 className='mb-2 text-xl font-bold font-sans md:text-4xl'>Himanshu Kumar</h1>
+                    <h3 className='font-normal  md:text-xl'>
+                        Full Stack Developer               
+                    </h3>
+                    <p className='pt-3'>Currently looking for new roles.</p>
+                    <p>(Ex: LoanWallah)</p>
+                    <p className='underline text-gray-400 pt-[2px]'> <a href="https://drive.google.com/file/d/1x2cvzlELKz68cITEil8L02wVVQHNa7RA/view?usp=sharing" target='_blank.'>Resume*</a></p>
+                    <br></br>
+
+                    <p className='text-sm md:text-base'>
+                        Building with MERN Stack, Javascript, Tailwind css⚡️
+                    </p>
+
+                </article>
+            </section>
+            <section className='w-3/5 md:w-2/5 flex flex-row items-start justify-between md:justify-evenly mt-12 md:mt-24'>
+                <a href='https://github.com/Robo707' target='_blank.' className='text-center text-3xl md:text-4xl transition-colors ease-in hover:bg-purple-600 rounded'>
+                    <Github />
+                </a>
+                <a href='https://leetcode.com/Himanshu7_07/' target='_blank.' className='text-center text-3xl md:text-4xl transition-colors ease-in hover:bg-purple-600 rounded'>
+                    <Leetcode />
+                </a>
+                <a href='https://www.codechef.com/users/himanshu707' target='_blank.' className='text-center text-3xl md:text-4xl transition-colors ease-in hover:bg-purple-600 rounded'>
+                    <Codechef />
+                </a>
+                <a href='https://www.linkedin.com/in/himanshush70a' target='_blank.' className='text-center text-3xl md:text-4xl transition-colors ease-in hover:bg-purple-600 rounded'>
+                    <Linkedin className='object-cover' />
+                </a>
+                <a href='https://twitter.com/Himanshu_887' target='_blank.' className='text-center text-3xl md:text-4xl transition-colors ease-in hover:bg-purple-600 rounded'>
+                    <Twitter className='object-cover' />
+                </a>
+            </section>
+        </motion.div>
+    )
+}
+
+export default Home
